fd5c73a8bb5ec1e3fcd0b23cd64882d09a9446ad
Hunter John Larco <hjlarco@gmail.com>
2015-02-06 20:10:20 -0500

    Sign Up Concept Created

--- app.py
+++ app.py
@@ -17,7 +17,15 @@ class LoginHandler(webapp2.RequestHandler):
     self.response.out.write(template.render(path, template_values))
 
 
+class SignupHandler(webapp2.RequestHandler):
+  def get(self):
+    template_values = {}
+    path = os.path.join(os.path.dirname(__file__), 'signup.html')
+    self.response.out.write(template.render(path, template_values))
+
+
 app = webapp2.WSGIApplication([
+                ('/signup/?', SignupHandler),
                 ('/login/?', LoginHandler),
                 ('/.*', LandingHandler)
               ], debug=True)
